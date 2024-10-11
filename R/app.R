6885b60f37378fb0ef7070aea3f72a31c3c5fce0
Richard White <richard.white@qmul.ac.uk>
2024-10-11 15:28:29 +0100

    Add data upload panel

--- R/app.R
+++ R/app.R
@@ -1,5 +1,6 @@
 library(shiny)
 library(r2d3)
+library(shinyModules)
 
 myApp <- function(testing = FALSE, debug = FALSE) {
   ui <- fluidPage(
@@ -9,25 +10,39 @@ myApp <- function(testing = FALSE, debug = FALSE) {
     ),
     navbarPage(
       "Cluster Viewer",
-      tabPanel("Data",
-               sidebarLayout(
-                 sidebarPanel(
-                   # File upload
-                   uploadGraphInput("GraphData")
-                 ),
-                 mainPanel(
-                   # file output for testing
-                   tableOutput("nodes"),
-                   tableOutput("edges")
-                 )
-               )
-      )
+      tabPanel(
+        "Data upload",
+        sidebarLayout(
+          sidebarPanel(
+            # File upload
+            h3("Graph Data:"),
+            uploadGraphInput("GraphData"),
+            # RNASeq upload
+            h3("RNA-seq Data:"),
+            uploadRNASeqInput("rnaseqData")
+          ),
+          mainPanel(
+            uploadRNASeqOutput("rnaseqData"),
+            # file output for testing
+            h3("Nodes:"),
+            tableOutput("nodes"),
+            h3("Edges:"),
+            tableOutput("edges")
+          )
+        )
+      ),
     )
   )
   server <- function(input, output, session) {
-    data_list <- uploadGraphServer("GraphData", debug)
+    data_list <- uploadGraphServer(id = "GraphData", debug = debug)
     output$nodes <- renderTable(data_list$nodes()[1:5,])
     output$edges <- renderTable(data_list$edges()[1:5,])
+
+    rnaseq_data_list <- uploadRNASeqServer(id = "rnaseqData", debug = debug)
+    output$samples <- renderTable(rnaseq_data_list$sample_info()[1:5,1:5])
+    output$counts <- renderTable(rnaseq_data_list$counts()[1:5,1:10])
+    output$metadata <- renderTable(rnaseq_data_list$gene_metadata()[1:5,])
+
   }
   shinyApp(ui, server)
 }
