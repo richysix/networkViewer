883f219cab3d87ace5c81af067a144628a1b16fb
Richard White <richard.white@qmul.ac.uk>
2025-01-03 17:44:34 +0000

    Change name to networkD3

--- R/network.R
+++ R/network.R
@@ -1,9 +1,9 @@
 #' Create UI components to upload node and edges files for a graph
 #'
-#' `networkOutput()` produces a d3 network visualisation of input data
+#' `networkD3Output()` produces a d3 network visualisation of input data
 #'
 #' @param id namespace id for the UI components. Must match the id provided to the
-#' [networkServer()] function.
+#' [networkD3Server()] function.
 #'
 #' @returns a [htmltools::tagList()] containing a [r2d3::d3Output()]
 #'
@@ -11,9 +11,9 @@
 #'
 #' @examples
 #'
-#' networkOutput("graph")
+#' networkD3Output("graph")
 #'
-networkOutput <- function(id) {
+networkD3Output <- function(id) {
   tagList(
     d3Output(NS(id, "d3_graph"))
   )
@@ -21,11 +21,11 @@ networkOutput <- function(id) {
 
 #' Server function to upload sample and count data files
 #'
-#' `networkServer()` implements uploading a sample file and a count data
+#' `networkD3Server()` implements uploading a sample file and a count data
 #' file. It also handles using the package test data.
 #'
 #' @param id namespace id for the UI components. Must match the id provided to the
-#' [networkOutput()] function.
+#' [networkD3Output()] function.
 #'
 #' @returns a list containing two [shiny::reactive()] objects
 #' * sampleInfo a data.frame of sample metadata
@@ -35,13 +35,21 @@ networkOutput <- function(id) {
 #'
 #' @examples
 #'
-#' networkServer("GraphData")
+#' networkD3Server("GraphData")
 #'
-networkServer <- function(id, nodes = NULL, edges = NULL, debug = FALSE) {
+networkD3Server <- function(id, nodes = NULL, edges = NULL, debug = FALSE) {
   stopifnot(is.reactive(nodes))
   stopifnot(is.reactive(edges))
+
   moduleServer(id, function(input, output, session) {
     output$d3_graph <- renderD3({
+      req(nodes())
+      req(edges())
+      if (debug) {
+        print(head(nodes()))
+        print(head(edges()))
+      }
+
       r2d3(
         data = list("nodes" = jsonlite::toJSON(nodes()),
                     "edges" = jsonlite::toJSON(edges())),
@@ -55,23 +63,23 @@ networkServer <- function(id, nodes = NULL, edges = NULL, debug = FALSE) {
 
 #' A test shiny app for the network module
 #'
-#' `networkApp()` creates a small test app for testing the [networkOutput()] and
-#' [networkServer()] functions.
+#' `networkD3App()` creates a small test app for testing the [networkD3Output()] and
+#' [networkD3Server()] functions.
 #'
 #' @return a [shiny::shinyApp()] object
 #'
 #' @examples
-#' networkApp()
-networkApp <- function(debug = FALSE) {
+#' networkD3App()
+networkD3App <- function(debug = FALSE) {
   ui <- fluidPage(
-        networkOutput("graph")
+        networkD3Output("graph")
   )
   server <- function(input, output, session) {
     node_data <- read_rds(file.path(here::here(), "tests", "testthat",
                                     "fixtures", "nodes-mini.rds"))
     edge_data <- read_rds(file.path(here::here(), "tests", "testthat",
                                     "fixtures", "edges-mini.rds"))
-    networkServer("graph", nodes = reactive(node_data),
+    networkD3Server("graph", nodes = reactive(node_data),
                   edges = reactive(edge_data), debug)
   }
   shinyApp(ui, server)
