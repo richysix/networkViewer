da15e85397469d517ddcbdaf75d162121d633c10
Richard White <richard.white@qmul.ac.uk>
2025-01-03 17:43:26 +0000

    Get bootstrap theme working

--- R/app.R
+++ R/app.R
@@ -1,14 +1,16 @@
 library(shiny)
 library(r2d3)
 library(shinyModules)
+library(bslib)
 
 myApp <- function(testing = FALSE, debug = FALSE) {
-  ui <- fluidPage(
-    theme = "flatly.bootstrap.min.css",
+  ui <- tagList(
     tags$head(
       tags$link(rel = "stylesheet", type = "text/css", href = "css/graph.css")
     ),
     navbarPage(
+      theme = bslib::bs_theme(bootswatch = "cosmo"),
+      inverse = TRUE,
       "Cluster Viewer",
       tabPanel(
         "Data upload",
@@ -23,6 +25,7 @@ myApp <- function(testing = FALSE, debug = FALSE) {
           ),
           mainPanel(
             # file output for testing
+            verbatimTextOutput("testing_msgs"),
             h3("Nodes:"),
             tableOutput("nodes"),
             h3("Edges:"),
@@ -33,16 +36,17 @@ myApp <- function(testing = FALSE, debug = FALSE) {
       ),
       tabPanel(
         "Gene Data",
-        sidebarLayout(
-          sidebarPanel(
-            countPlotInput("gene1"),
-            hr(),
-            countXYScatterPlotInput("xy_scatter")
-          ),
-          mainPanel(
-            h3("Count Plot"),
-            countPlotOutput("gene1"),
-            h3("XY scatterplot"),
+        card(
+          card_header("Count Plot"),
+          layout_sidebar(
+            sidebar = countPlotInput("gene1"),
+            countPlotOutput("gene1")
+          )
+        ),
+        card(
+          card_header("XY scatterplot"),
+          layout_sidebar(
+            sidebar = countXYScatterPlotInput(id = "xy_scatter", hide_x = TRUE),
             countXYScatterPlotOutput("xy_scatter")
           )
         )
@@ -50,11 +54,22 @@ myApp <- function(testing = FALSE, debug = FALSE) {
     )
   )
   server <- function(input, output, session) {
+    if (testing) {
+      output$testing_msgs <-
+        renderPrint({
+          glue::glue(
+            "In networkViewer app server function:\n",
+            "Bootstrap version is: ",
+            "{bslib::theme_version(shiny::getCurrentTheme())}")
+        })
+    }
+
     data_list <- uploadGraphServer(id = "GraphData", debug = debug)
     output$nodes <- renderTable(data_list$nodes()[1:5,])
     output$edges <- renderTable(data_list$edges()[1:5,])
 
-    rnaseq_data_list <- uploadRNASeqServer(id = "rnaseqData", debug = debug)
+    rnaseq_data_list <- uploadRNASeqServer(id = "rnaseqData", testing = testing,
+                                           debug = debug)
     output$samples <- renderTable(rnaseq_data_list$sample_info()[1:5,1:5])
     output$counts <- renderTable(rnaseq_data_list$counts()[1:5,1:10])
     output$metadata <- renderTable(rnaseq_data_list$gene_metadata()[1:5,])
