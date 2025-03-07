89a906a39deee2f95884704c02036e2c885bb92a
Richard White <richard.white@qmul.ac.uk>
2025-03-07 11:35:56 +0000

    Updates to graphD3 module
    Added svg_height argument to graphD3Output
    Added d3_options argument to graphD3Server

--- R/graphD3.R
+++ R/graphD3.R
@@ -13,7 +13,7 @@
 #'
 #' graphD3Output("graph")
 #'
-graphD3Output <- function(id, svg_height = 300) {
+graphD3Output <- function(id, svg_height = 800) {
   tagList(
     d3Output(NS(id, "d3_graph"), height = svg_height)
   )
@@ -37,9 +37,10 @@ graphD3Output <- function(id, svg_height = 300) {
 #'
 #' graphD3Server("GraphData")
 #'
-graphD3Server <- function(id, nodes = NULL, edges = NULL, debug = FALSE) {
+graphD3Server <- function(id, nodes = NULL, edges = NULL, d3_options = NULL, debug = FALSE) {
   stopifnot(is.reactive(nodes))
   stopifnot(is.reactive(edges))
+  stopifnot(!is.reactive(d3_options))
 
   moduleServer(id, function(input, output, session) {
     output$d3_graph <- renderD3({
@@ -56,7 +57,12 @@ graphD3Server <- function(id, nodes = NULL, edges = NULL, debug = FALSE) {
         script = file.path(here::here(), "www", "js", "graph-svg.js"),
         css = file.path(here::here(), "www", "css", "graph.css"),
         d3_version = "6",
-        height = "100%"
+        options = d3_options,
+        # this does not work
+        sizing = htmlwidgets::sizingPolicy(
+          # padding = 0, browser.fill = TRUE,
+          defaultHeight = "800px"
+        )
       )
     })
   })
@@ -71,17 +77,26 @@ graphD3Server <- function(id, nodes = NULL, edges = NULL, debug = FALSE) {
 #'
 #' @examples
 #' graphD3App()
-graphD3App <- function(debug = FALSE) {
+graphD3App <- function(debug = TRUE, use_size = FALSE, use_weight = FALSE,
+                       scale_weights = FALSE, colour_nodes = FALSE) {
   ui <- fluidPage(
         graphD3Output("graph")
   )
   server <- function(input, output, session) {
-    node_data <- read_rds(file.path(here::here(), "tests", "testthat",
+    node_data <- readr::read_rds(file.path(here::here(), "tests", "testthat",
                                     "fixtures", "nodes-mini.rds"))
-    edge_data <- read_rds(file.path(here::here(), "tests", "testthat",
+    edge_data <- readr::read_rds(file.path(here::here(), "tests", "testthat",
                                     "fixtures", "edges-mini.rds"))
-    graphD3Server("graph", nodes = reactive(node_data),
-                  edges = reactive(edge_data), debug)
+    graphD3Server(
+      "graph",
+      nodes = reactive(node_data),
+      edges = reactive(edge_data),
+      d3_options = list("use_size" = use_size,
+                        "use_weight" = use_weight,
+                        "scale_weights" = scale_weights,
+                        "colour_nodes" = colour_nodes),
+      debug
+    )
   }
   shinyApp(ui, server)
 }
